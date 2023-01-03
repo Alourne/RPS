4039de59bfe74e4807720fcc635c502f69ba7ec8
Alourne <120846738+Alourne@users.noreply.github.com>
2023-01-03 16:55:57 -0500

    working on script

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors"; 
+    let getRandomValue = Math.random();
+    // console.log(getRandomValue);
+
+    if (getRandomValue <= 0.33) {
+        //console.log(rock);
+        return rock;
+    }
+    else if (getRandomValue <= 0.66) {
+        //console.log(paper);
+        return paper;
+    }
+    else {
+        //console.log(scissors);
+        return scissors;
+    }
+}
+
+function playRound(playerSelection, ComputerSelection) {
+    let win = "You win! You selected" + playerSelection + " and the computer chose " + ComputerSelection;
+    let tie = "It's a tie! You selected" + playerSelection + " and the computer chose " + ComputerSelection;
+    let lose = "You lost! You selected" + playerSelection + " and the computer chose " + ComputerSelection;
+}
